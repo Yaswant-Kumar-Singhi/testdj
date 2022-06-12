1938eaf369d62aeac02cd69386850ef382253f19
YASWANT KUMAR SINGHI <yaswant.kumar.singhi.fiem.it16@teamfuture.in>
2022-06-12 14:09:12 +0530

    updating seetings

--- testdj/settings.py
+++ testdj/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['newmyapph.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['newmyapph.herokuapp.com','localhost','127.0.0.1']
 #ALLOWED_HOSTS = ["*"]
 
 
